6ac54f1e230210b9e4edbdbe0ea4ac1f885b4243
vidhanpatel100-web <vidhanpatel100@gmail.com>
2026-07-01 16:10:36 +0530

    Update init_database.sql

--- Script/init_database.sql
+++ Script/init_database.sql
@@ -1,1 +1,52 @@
+/*
+===========================================================
+Create Database and Schemas
+===========================================================
+
+Script Purpose:
+    This script creates a new database named '[E-commerce]' after checking if it already exists.
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+    within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this script will drop the entire '[E-commerce]' database if it exists.
+    All data in the database will be permanently deleted. Proceed with caution
+    and ensure you have proper backups before running this script.
+*/
+
+
+
+USE master;
+GO
+
+-- Drop and recreate the '[E-commerce]' database
+IF EXISTS (
+    SELECT 1
+    FROM sys.databases
+    WHERE name = '[E-commerce]'
+)
+BEGIN
+    ALTER DATABASE [E-commerce]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE [E-commerce];
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE [E-commerce];
+GO
+
+USE [E-commerce];
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
 
