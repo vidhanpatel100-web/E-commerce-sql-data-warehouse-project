52e46b1acf92e779f1ca213382826e0efc491e0e
vidhanpatel100-web <vidhanpatel100@gmail.com>
2026-07-20 10:23:07 +0530

    Update 10_product_price_tier_distribution.sql

--- Script/Analytics.sql/10_product_price_tier_distribution.sql
+++ Script/Analytics.sql/10_product_price_tier_distribution.sql
@@ -7,29 +7,22 @@
 
 WITH ProductSegments AS (
     -- Step 1: Isolate products and group pricing structures from the warehouse catalog
-    SELECT
-        fs.product_id,
-        dp.product_category,
-        fs.item_price,
-        CASE 
-            WHEN fs.item_price < 50 THEN 'Below 50'
-            WHEN fs.item_price BETWEEN 50 AND 100 THEN '50-100'
-            WHEN fs.item_price BETWEEN 100 AND 500 THEN '100-500'
-            ELSE 'Above 500'
-        END AS cost_range
-    FROM 
-        gold.fact_sales fs
-    LEFT JOIN 
-        gold.dim_product dp ON fs.product_id = dp.product_id
-)
+SELECT
+product_id,
+product_category,
+item_price,
+CASE WHEN item_price < 50 THEN 'Below 50'
+	WHEN item_price  BETWEEN 50 AND 100 THEN '50-100'
+	WHEN item_price BETWEEN 100 AND 500 THEN '100-500'
+	ELSE 'Above 500'
+END cost_range
+FROM gold.main_table)
 -- Step 2: Evaluate distinct catalog density across pricing tiers
 SELECT
-    ps.cost_range,
-    COUNT(DISTINCT ps.product_id) AS total_distinct_products,
-    COUNT(ps.product_id) AS total_transactional_items_sold
-FROM 
-    ProductSegments ps
-GROUP BY 
-    ps.cost_range
-ORDER BY 
-    total_distinct_products DESC;
+cost_range,
+COUNT(product_id) as total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC
+
+
